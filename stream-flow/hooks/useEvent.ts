c0c378655c371fcc144e2ccc09f85642f4952982
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-01-17 01:28:55 +0300

    (feat, events): Add hook for events

---
+++ stream-flow/hooks/useEvent.ts
@@ -0,0 +1,46 @@
+import { RefObject, useEffect } from "react";
+import createHandlerSetter from "./factory/createHandlerSetter";
+import safeHasOwnProperty from "../utils/safeHasOwnProperty";
+
+/**
+ * Accepts the reference to an HTML Element and an event name then performs the necessary operations to listen to the event
+ * when fired from that HTML Element.
+ */
+const useEvent = <
+  TEvent extends Event,
+  TElement extends HTMLElement = HTMLElement
+>(
+  ref: RefObject<TElement>,
+  eventName: string,
+  options?: AddEventListenerOptions
+) => {
+  const [handler, setHandler] = createHandlerSetter<TEvent>();
+
+  if (!!ref && !safeHasOwnProperty(ref, "current")) {
+    throw new Error("Unable to assign any scroll event to the given ref");
+  }
+
+  useEffect(() => {
+    // @ts-ignore
+    const cb: EventListenerOrEventListenerObject = (event: TEvent) => {
+      if (handler.current) {
+        handler.current(event);
+      }
+    };
+
+    if (ref.current && ref.current.addEventListener && handler.current) {
+      ref.current.addEventListener(eventName, cb, options);
+    }
+
+    return () => {
+      // @ts-ignore
+      if (ref.current && ref.current.addEventListener && handler.current) {
+        ref.current.removeEventListener(eventName, cb, options);
+      }
+    };
+  }, [eventName, ref.current, options]);
+
+  return setHandler;
+};
+
+export default useEvent;
