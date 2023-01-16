2764b21d3830fc1916a3eb8f694d1080b06044f0
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-01-17 01:15:30 +0300

    (init, models): Add hook utils models

---
+++ stream-flow/models/types.ts
@@ -0,0 +1,23 @@
+/**
+ * Represent a generic function.
+ * Used internally to improve code readability
+ */
+export interface GenericFunction {
+  (...args: any[]): any;
+}
+
+/**
+ * Typed generic callback function, used mostly internally
+ * to defined callback setters
+ */
+export interface SomeCallback<TArgs, TResult = void> {
+  (...args: TArgs[]): TResult;
+}
+
+/**
+ * A callback setter is generally used to set the value of
+ * a callback that will be used to perform updates
+ */
+export interface CallbackSetter<TArgs> {
+  (nextCallback: SomeCallback<TArgs>): void;
+}
