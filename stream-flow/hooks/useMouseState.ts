e3929caf8bec8a55c87bc86c294d2c1f53e70db2
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-01-17 01:33:28 +0300

    (feat, hooks): Add mouse state hook

---
+++ stream-flow/hooks/useMouseState.ts
@@ -0,0 +1,35 @@
+import { RefObject, useState } from "react";
+import useMouseEvents from "./useMouseEvents";
+
+const createStateObject = (event: MouseEvent) => ({
+  clientX: event.clientX,
+  clientY: event.clientY,
+  screenX: event.screenX,
+  screenY: event.screenY,
+});
+
+/**
+ * Returns the current state (position) of the mouse pointer.
+ * It possibly accepts a DOM ref representing the mouse target.
+ * If a target is not provided the state will be caught globally.
+ */
+const useMouseState = <TElement extends HTMLElement>(
+  targetRef?: RefObject<TElement>
+) => {
+  const [state, setState] = useState({
+    clientX: 0,
+    clientY: 0,
+    screenX: 0,
+    screenY: 0,
+  });
+  const { onMouseMove } = useMouseEvents<TElement>(targetRef);
+
+  onMouseMove((event: MouseEvent) => {
+    const nextState = createStateObject(event);
+    setState(nextState);
+  });
+
+  return state;
+};
+
+export default useMouseState;
