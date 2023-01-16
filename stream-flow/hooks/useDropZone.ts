a13f9c285051021b74954aa3fec74b3d1ba4da06
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-01-17 02:04:14 +0300

    (feat, hooks): Add hooks for the drop zones

---
+++ stream-flow/hooks/useDropZone.ts
@@ -0,0 +1,25 @@
+import { RefObject, useState } from "react";
+import useDragEvents from "./useDragEvents";
+
+const useDropZone = <TElement extends HTMLElement>(
+  targetRef: RefObject<TElement>
+) => {
+  const { onDrop, onDragOver, onDragLeave } = useDragEvents<TElement>(
+    targetRef,
+    false
+  );
+  const [isOver, setIsOver] = useState(false);
+
+  onDragOver((event: DragEvent) => {
+    event.preventDefault();
+    setIsOver(true);
+  });
+
+  onDragLeave(() => {
+    setIsOver(false);
+  });
+
+  return { isOver, onDrop };
+};
+
+export default useDropZone;
