842daf4054f7aad79d76efd673eb6c9423a35914
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-01-17 01:35:13 +0300

    (feat, hooks): Combine both mouse hooks into a single hook

---
+++ stream-flow/hooks/useMouse.ts
@@ -0,0 +1,19 @@
+import { RefObject } from "react";
+import useMouseEvents from "./useMouseEvents";
+import useMouseState from "./useMouseState";
+
+/**
+ * Returns an array where the first item is the mouse state from the `useMouseState` hook and the second item
+ * is the object of callback setters from the `useMouseEvents` hook.
+ * It is intended as a shortcut to those hooks.
+ */
+const useMouse = <TElement extends HTMLElement>(
+  targetRef?: RefObject<TElement>
+) => {
+  const state = useMouseState<TElement>(targetRef);
+  const events = useMouseEvents<TElement>(targetRef);
+
+  return [state, events];
+};
+
+export default useMouse;
